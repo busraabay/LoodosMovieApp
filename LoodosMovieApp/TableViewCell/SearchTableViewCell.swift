766f652e1b49d98cde51e5c525db000aee347713
Büşra <busra_abay@aof.anadolu.edu.tr>
2022-03-17 16:00:06 +0300

    searchTableViewCell created

---
+++ LoodosMovieApp/TableViewCell/SearchTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  SearchTableViewCell.swift
+//  LoodosMovieApp
+//
+//  Created by Büşra on 17.03.2022.
+//
+
+import UIKit
+import Kingfisher
+
+class SearchTableViewCell: UITableViewCell {
+
+    static let identifier = "SearchTableViewCell"
+    static func nib() -> UINib {
+        return UINib(nibName: "SearchTableViewCell", bundle: nil)
+    }
+ 
+    @IBOutlet weak var moviesNameLabel: UILabel!
+    @IBOutlet weak var moviesImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+    func configureCell(search: SearchMovie) {
+        moviesNameLabel.text = search.title?.uppercased()
+        moviesImageView.kf.setImage(with: URL(string: search.poster ?? ""))
+        
+    }
+    
+}
--- LoodosMovieApp/ViewControllers/ViewController.swift
+++ LoodosMovieApp/ViewControllers/ViewController.swift
@@ -7,13 +7,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchTableView: UITableView!
+    
+    var networkManager = NetworkManager()
+    var searchMovie: Movie?
+    var movie: SearchMovie?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTableView.register(SearchTableViewCell.nib(), forCellReuseIdentifier: SearchTableViewCell.identifier)
+        searchTableView.delegate = self
+        searchTableView.dataSource = self
+        
+//        loadSearchMovies(title: "spider")
+        
+        searchBar.delegate = self
+        searchBar.placeholder = "Search Movie"
     }
 
+    func loadSearchMovies(title: String) {
+        networkManager.fetchSearchMovies(title: title) { [weak self] result in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let response):
+                strongSelf.searchMovie = response
+                strongSelf.searchTableView.reloadData()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchMovie?.search?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell {
+            if let movieName = searchMovie?.search?[indexPath.row]{
+                cell.configureCell(search: movieName)
+            }
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        tableView.deselectRow(at: indexPath, animated: true)
+//        Analytics.logEvent("movieDetail", parameters: ["name": movie?.title, "id": movie?.imdbID])
+//        let selectedMovie = searchMovie?.search?[indexPath.row]
+//        let storyboard = UIStoryboard(name: "Detail", bundle: nil)
+//        let vc = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+//        vc.modalPresentationStyle = .fullScreen
+//        vc.id = selectedMovie?.imdbID
+//        show(vc, sender: nil)
+//    }
+}
 
+//MARK: - SearchBar Delegate
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+         searchBar.endEditing(true)
+    }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard let query = searchBar.text else { return }
+        loadSearchMovies(title: query)
+
+        DispatchQueue.main.async {
+            self.searchTableView.reloadData()
+        }
+    }
 }
 
